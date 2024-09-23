4a3c210d8141187d2116911e893cb97b6818b193
StevenSungStudent <S.Sung@student.han.nl>
2024-09-23 14:35:50 +0200

    updated build script

--- build_smartcar.bash
+++ build_smartcar.bash
@@ -1,5 +1,5 @@
 colcon clean workspace -y
 
 xacro src/smart_car/urdf/smartcar.urdf.xacro > src/smart_car/urdf/smartcar.urdf
-colcon build --packages-select smart_car
+colcon build --packages-select smart_car smartcar_msg
 . install/setup.sh
